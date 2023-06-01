dd284393d852afebf6119fe9b7d73fb5e4a59540
YellowySmile979 <xcant5@gmail.com>
2023-06-01 10:16:07 +0800

    bug fixed wishing well

--- The Light of Gaben/Assets/Scripts/Gacha/GachaWishingWell.cs
+++ The Light of Gaben/Assets/Scripts/Gacha/GachaWishingWell.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 public class GachaWishingWell : MonoBehaviour
 {
     public GameObject gachaScreen;
+    bool hasCollided;
 
     //when player collides, open the gacha screen
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.GetComponent<PlayerMovement>())
+        if (collision.GetComponent<PlayerMovement>() && !hasCollided)
         {
             Time.timeScale = 0;
             gachaScreen.SetActive(true);
+            hasCollided = true;
+        }
+    }
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.GetComponent<PlayerMovement>())
+        {
+            hasCollided = false;
         }
     }
 }
