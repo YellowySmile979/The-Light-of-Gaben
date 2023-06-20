219bdb34cc46b34267ee2d8f50c411a7b0da0db4
YellowySmile979 <xcant5@gmail.com>
2023-06-20 13:35:23 +0800

    bug fixing

--- The Light of Gaben/Assets/Scripts/Combat/PlayerCombatController.cs
+++ The Light of Gaben/Assets/Scripts/Combat/PlayerCombatController.cs
@@ -10,6 +10,13 @@ public class PlayerCombatController : UnitStats
     public float playerLevel = 1;
     public float playerXP = 0;
 
+    public static PlayerCombatController Instance;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
     void Update()
     {
         // Controls XP levelling
@@ -98,5 +105,91 @@ public class PlayerCombatController : UnitStats
     {
         lightType = LightTypes.White;
         stateController.actionDesc = "Player changes their light to White!";
+    }
+    public void UpdatePlayerLevel(float xpGiven)
+    {
+        playerXP += xpGiven;
+        print("playerXP: " + playerXP);
+        if (playerXP >= 0 && playerXP < 200)
+        {
+            level = 1;
+        }
+        else if (playerXP >= 200 && playerXP < 330)
+        {
+            level = 2;
+        }
+        else if (playerXP >= 330 && playerXP < 460)
+        {
+            level = 3;
+        }
+        else if (playerXP >= 460 && playerXP < 590)
+        {
+            level = 4;
+        }
+        else if (playerXP >= 590 && playerXP < 620)
+        {
+            level = 5;
+        }
+        else if (playerXP >= 620 && playerXP < 770)
+        {
+            level = 6;
+        }
+        else if (playerXP >= 770 && playerXP < 920)
+        {
+            level = 7;
+        }
+        else if (playerXP >= 920 && playerXP < 1100)
+        {
+            level = 8;
+        }
+        else if (playerXP >= 1100 && playerXP < 1280)
+        {
+            level = 9;
+        }
+        else if (playerXP >= 1280 && playerXP < 1500)
+        {
+            level = 10;
+        }
+        else if (playerXP >= 1500 && playerXP < 1720)
+        {
+            level = 11;
+        }
+        else if (playerXP >= 1720 && playerXP < 1940)
+        {
+            level = 12;
+        }
+        else if (playerXP >= 1940 && playerXP < 2210)
+        {
+            level = 13;
+        }
+        else if (playerXP >= 2210 && playerXP < 2480)
+        {
+            level = 14;
+        }
+        else if (playerXP >= 2480 && playerXP < 2750)
+        {
+            level = 15;
+        }
+        else if (playerXP >= 2750 && playerXP < 3050)
+        {
+            level = 16;
+        }
+        else if (playerXP >= 3050 && playerXP < 3350)
+        {
+            level = 17;
+        }
+        else if (playerXP >= 3350 && playerXP < 3950)
+        {
+            level = 18;
+        }
+        else if (playerXP >= 3950 && playerXP < 4550)
+        {
+            level = 19;
+        }
+        else if (playerXP >= 4550)
+        {
+            level = 20;
+        }
+        GeneralCanvasStuff.Instance.UpdateLevelText(level);
     }
 }
